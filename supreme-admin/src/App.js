96b79ec53c4c931bd9b5c2ae6fd4596465236d1e
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-02 16:55:13 +0530

    Admin signin button added

--- supreme-admin/src/App.js
+++ supreme-admin/src/App.js
@@ -1,33 +1,29 @@
+import React from 'react';
+import {Nav, NavDropdown, Navbar} from 'react-bootstrap';
 import './App.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'; //importing jumbotron file from react-bootstrap folder
 
 function App() {
   return (
     <div className="App">
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      </NavDropdown> */}
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+      <Nav.Link href="#deets">Sign-in</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-        </div>
+    </div>
   );
 }
 
