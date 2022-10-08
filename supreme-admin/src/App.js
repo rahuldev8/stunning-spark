f5e6adcc5eac4e253ec363e9bfd611fcfa57eba7
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-08 17:44:29 +0530

    Based on Url, navigate to home,signin and up

--- supreme-admin/src/App.js
+++ supreme-admin/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Layout from './components/Layout';
+import Home from './containers/Home/index';
+import Signin from './containers/Signin/index';
+import Signup from './containers/Signup/index';
+
 
 function App() {
   return (
     <div className="App">
-      
-    <Layout>
-      <h1>Hello Admin</h1>
-    </Layout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </Router>
 
     </div>
   );
