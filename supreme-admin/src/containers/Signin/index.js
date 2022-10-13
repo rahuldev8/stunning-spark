508356a1a203b6c3e719ec7c7c99c54074348d17
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-13 22:06:28 +0530

    Signin Form Created

--- supreme-admin/src/containers/Signin/index.js
+++ supreme-admin/src/containers/Signin/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Input from '../../components/UI/Input';
 
 /**
 * @author
@@ -14,18 +15,20 @@ export const Signin = (props) => {
         <Row style={{marginTop: '75px'}}>
           <Col md={{span:6, offset: 3}}>
             <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+            <Input 
+                  label = "Email"
+                  placeholder = "Email"
+                  value=""
+                  type="text"
+                  onChange={() => {}}
+                  />
+              <Input 
+                  label = "Password"
+                  placeholder = "Password"
+                  value=""
+                  type="text"
+                  onChange={() => {}}
+                  />
               <Button variant="primary" type="submit">
                 Submit
               </Button>
