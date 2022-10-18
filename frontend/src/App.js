1df4a8d953e3221d195bb1264bbe93409533a6ca
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-18 21:24:31 +0530

    Morph CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
+const App =() => {
   return (
     <>
-     <h1>Welcome to Stunning Spark</h1> 
+    <Header/>
+    <main>
+      <h1>Welcome to Stunning Spark</h1> 
+    </main>
+    <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        Header
+    </header>
+  )
+}
+
+export default Header
