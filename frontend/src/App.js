9dffec553989d965410e856468b064399199455f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-18 23:52:21 +0530

    HomeScreen Path Routed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,15 +7,15 @@ import HomeScreen from './screens/HomeScreen';
 
 const App =() => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
