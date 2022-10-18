85ee795aa602d0dbeb0ee8d58562dd42b82ec569
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-18 22:34:37 +0530

    Test Products Listed with All infos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App =() => {
   return (
     <>
-
-    <main>
-    <Header/>
-      <Container>
-        <h1>Welcome to Stunning Spark</h1>
-      </Container> 
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
