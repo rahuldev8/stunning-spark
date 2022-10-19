5051d52ddb0c5f07a5f917851ed3d96293f759a8
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-19 18:08:11 +0530

    Cart Screen and Functionality Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/cartScreen';
 
 const App =() => {
   return (
@@ -14,6 +15,7 @@ const App =() => {
         <Container>
           <Route path='/' component={HomeScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} exact/>
+          <Route path='/cart/:id?' component={CartScreen} exact/>
         </Container>
       </main>
       <Footer />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { productListReducer,productDetailsReducer, } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 
 const middleware = [thunk]
 
