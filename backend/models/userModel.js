39359b4588f768edd1581acb951d0411f9fa7bff
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-19 18:21:05 +0530

    Created Product Controllers

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -26,6 +27,9 @@ const userSchema = mongoose.Schema(
   }
 )
 
-const User = mongoose.model('User', userSchema)
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+  }
 
+const User = mongoose.model('User', userSchema)
 export default User
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -17,6 +18,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
