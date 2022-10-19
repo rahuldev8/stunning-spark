48665889ef82b5e3dd6c6aa987246c5d5292220f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-19 17:49:24 +0530

    redux state for productScreens

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer,productDetailsReducer, } from './reducers/productReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 
 const initialState = {}
