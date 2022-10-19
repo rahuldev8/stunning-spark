93daa661050f80ddd8636d2e94b6353b43ef01c3
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-19 14:13:02 +0530

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
  import express from 'express'
  import dotenv from 'dotenv'
+ import connectDB from './config/db.js'
  import products from './Data/products.js'
 
  dotenv.config()
+ connectDB()
  const app = express()
 
  app.get('/', (req, res) => { 
