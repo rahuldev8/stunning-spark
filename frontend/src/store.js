25d0c77524782fa3e689ed8f6e9c1ae8edc1e4f9
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-19 17:28:59 +0530

    redux state to Homescreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { productListReducer } from './reducers/productReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 
 const initialState = {}
 
