4f9841df50e62a60e5f9696601ea6b0bfabd123f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 10:56:20 +0530

    Password encryt using brcypt salt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,5 +26,64 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Register a New User
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+  
+    const userExists = await User.findOne({ email })
 
-export { authUser }
+    if(userExists)
+    {
+        res.status(400)
+        throw new Error('User Already Registered')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user)
+    {
+        res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+
+        })
+    }
+    else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+  
+  })
+
+
+// @desc    get user profile
+// @route   GET /api/users/profile
+// @access  Public
+const getUserProfile = asyncHandler(async (req, res) =>  {
+    const user = await User.findById(req.user._id)
+
+    if(user)
+    {
+        res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        })
+    }else{
+        res.status(404)
+        throw new Error('User Not Found')
+    }
+  })
+
+
+export { authUser, registerUser, getUserProfile }
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,33 +1,31 @@
 import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
+import expressAsyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
-const protect = asyncHandler(async (req, res, next) => {
+const protect = expressAsyncHandler(async (req, res, next) => {
   let token
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+  {
     try {
-      token = req.headers.authorization.split(' ')[1]
+        token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        req.user = await User.findById(decoded.id).select('-password')
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-      req.user = await User.findById(decoded.id).select('-password')
-
-      next()
+        next()
     } catch (error) {
-      console.error(error)
-      res.status(401)
-      throw new Error('Not authorized, token failed')
+        console.error(error)
+        res.status(401)
+        throw new Error('Not Authorized, Token Failed..')
+        
     }
   }
-
-  if (!token) {
+  if(!token)
+  {
     res.status(401)
-    throw new Error('Not authorized, no token')
+    throw new Error('Not Authorized, No token')
   }
 })
 
-export { protect }
+export default protect
