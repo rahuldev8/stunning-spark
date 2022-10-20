222a1404122acffdef15d3009812bdaef9b86266
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 13:43:39 +0530

    Login Screen and Functionality Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/cartScreen';
+import LoginScreen from './screens/loginScreen';
 
 const App =() => {
   return (
@@ -13,9 +14,10 @@ const App =() => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/login' component={LoginScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/cart/:id?' component={CartScreen} exact/>
+          <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
       <Footer />
