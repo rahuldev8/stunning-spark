0eb8e5a6660d0ed00db28a976478918a53511a0a
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 21:12:02 +0530

    Admin Page Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App =() => {
   return (
@@ -29,6 +30,7 @@ const App =() => {
           <Route path='/profile' component={ProfileScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/cart/:id?' component={CartScreen} exact/>
+          <Route path='/admin/userlist' component={UserListScreen} exact/>
           <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
