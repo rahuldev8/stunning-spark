ae20a7503cf37e5355c4fdf4192c4ee051182c34
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 14:04:06 +0530

    user Register Screen and Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/cartScreen';
+import RegisterScreen from './screens/RegisterScreen'
 import LoginScreen from './screens/loginScreen';
 
 const App =() => {
@@ -15,6 +16,7 @@ const App =() => {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen} exact/>
+          <Route path='/register' component={RegisterScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/cart/:id?' component={CartScreen} exact/>
           <Route path='/' component={HomeScreen} exact/>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { productListReducer,productDetailsReducer, } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer } from './reducers/userReducer'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
@@ -10,6 +10,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
