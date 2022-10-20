44a082642f70913688a163278ac065351afad60e
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 21:53:11 +0530

    User Profile Edit by Admin Implemented

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -20,6 +20,10 @@ import {
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
     USER_DELETE_FAIL,
+    USER_UPDATE_RESET,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
   } from '../constants/userConstants'
   
   export const userLoginReducer = (state = {}, action) => {
@@ -105,3 +109,20 @@ import {
         return state
     }
   } 
+
+  export const userUpdateReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+      case USER_UPDATE_REQUEST:
+        return { loading: true }
+      case USER_UPDATE_SUCCESS:
+        return { loading: false, success: true }
+      case USER_UPDATE_FAIL:
+        return { loading: false, error: action.payload }
+      case USER_UPDATE_RESET:
+        return {
+          user: {},
+        }
+      default:
+        return state
+    }
+  }  
