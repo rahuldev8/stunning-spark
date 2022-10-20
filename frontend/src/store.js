31be0fc1ad0c7c2ec181a0a61669f19a0cdf9171
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 17:15:53 +0530

    Create Order Function Implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,8 @@ import {
   userUpdateProfileReducer,
 } from './reducers/userReducer.js'
 
+import { orderCreateReducer } from './reducers/orderReducers'
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
@@ -21,6 +23,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
