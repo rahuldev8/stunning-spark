1f13585e10e7c790ee7dc709c3a6e2231954f428
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-20 21:45:10 +0530

    User Profile Edit by Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 const App =() => {
   return (
@@ -31,6 +32,7 @@ const App =() => {
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/cart/:id?' component={CartScreen} exact/>
           <Route path='/admin/userlist' component={UserListScreen} exact/>
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} exact/>
           <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
