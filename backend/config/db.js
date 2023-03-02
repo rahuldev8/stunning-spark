79873e15d03391c68dee7eac8537712ad633276f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2023-03-02 12:51:38 +0530

    strictQuery on DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
     })
 
