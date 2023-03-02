bbebe0dc30105ff71566bd86f293b9f0046dff0a
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2023-03-02 12:33:01 +0530

    Standalone backend Server

--- backend/server.js
+++ backend/server.js
@@ -54,11 +54,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  console.log( 
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
